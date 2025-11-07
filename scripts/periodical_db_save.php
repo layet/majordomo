25c217af5ca75204b5372890541797c5e4d68751
Sergei Jeihala <sergejey@gmail.com>
2025-11-07 19:06:48 +0200

    Update periodical_db_save.php

--- scripts/periodical_db_save.php
+++ scripts/periodical_db_save.php
@@ -49,7 +49,7 @@ if (defined('PATH_TO_MYSQLDUMP')) {
         $mysqlDumpPath = "/usr/bin/mysqldump";
 }
 
-$mysqlDumpParam = " -h " . DB_HOST . " --user=" . DB_USER . " --password=" . DB_PASSWORD;
+$mysqlDumpParam = " -h " . DB_HOST . " --user=\"" . DB_USER . "\" --password=\"" . DB_PASSWORD."\"";
 $mysqlDumpParam .= " --no-create-db --add-drop-table " . DB_NAME;
 
 $backups_in_row = 0;
