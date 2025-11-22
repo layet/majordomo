b01b2570aa6df036a636f5a15e0c7e595691e857
Andrei Trybukhouski <Andrei.Trybukhouski@gmail.com>
2025-11-22 18:25:13 +0200

    fix websocket server

--- lib/websockets/server/lib/WebSocket/Server.php
+++ lib/websockets/server/lib/WebSocket/Server.php
@@ -10,6 +10,12 @@ namespace WebSocket;
 class Server extends Socket
 {   
     protected $clients = array();
+        /**
+         * map of client socket id => last activity timestamp
+         * used for timeout/disconnect of stalled clients
+         * @var array
+         */
+        protected $clientActivity = array();
     protected $applications = array();
         private $_ipStorage = array();
         private $_requestStorage = array();
@@ -20,12 +26,25 @@ class Server extends Socket
         private $_maxClients = 30;
         private $_maxConnectionsPerIp = 5;
         private $_maxRequestsPerMinute = 50;
+        /**
+         * Timeout for inactive clients in seconds
+         */
+        private $_clientTimeout = 30;
 
     public function __construct($host = 'localhost', $port = 8000, $ssl = false)
     {
         parent::__construct($host, $port, $ssl);
         $this->log('Server created');
     }
+
+        /**
+         * Set client inactivity timeout (seconds)
+         * @param int $seconds
+         */
+        public function setClientTimeout($seconds)
+        {
+                $this->_clientTimeout = (int)$seconds;
+        }
 
     /**
      * Creates a connection from a socket resource
@@ -41,86 +60,152 @@ class Server extends Socket
         /**
          * Main server method. Listens for connections, handles connectes/disconnectes, e.g.
          */
-        public function run()
-        {
-                while(true)
-                {
-                        $changed_sockets = $this->allsockets;
-                        $write = null;
-                        $except = null;
-                        @stream_select($changed_sockets, $write, $except, 0, 5000);                      
-                        foreach($changed_sockets as $socket)
-                        {
-                                if($socket == $this->master)
-                                {
-                                        if(($ressource = stream_socket_accept($this->master)) === false)
-                                        {
-                                                $this->log('Socket error: ' . socket_strerror(socket_last_error($ressource)));
-                                                continue;
-                                        }
-                                        else
-                                        {
-                                                $client = $this->createConnection($ressource);
-                                                $this->clients[(int)$ressource] = $client;
-                                                $this->allsockets[] = $ressource;
-                                                
-                                                if(count($this->clients) > $this->_maxClients)
-                                                {
-                                                        $this->log('Attention: Client Limit Reached!');
-                                                        $client->onDisconnect();
-                                                        if($this->getApplication('status') !== false)
-                                                        {
-                                                                $this->getApplication('status')->statusMsg('Attention: Client Limit Reached!', 'warning');
-                                                        }
-                                                        continue;
-                                                }
-                                                
-                                                $this->_addIpToStorage($client->getClientIp());
-                                                if($this->_checkMaxConnectionsPerIp($client->getClientIp()) === false)
-                                                {
-                                                        $this->log('Connection/Ip limit for ip ' . $client->getClientIp() . ' was reached!');
-                                                        $client->onDisconnect();
-                                                        if($this->getApplication('status') !== false)
-                                                        {
-                                                                $this->getApplication('status')->statusMsg('Connection/Ip limit for ip ' . $client->getClientIp() . ' was reached!', 'warning');
-                                                        }
-                                                        continue;
-                                                }                                               
-                                        }
+public function run()
+{
+    while (true) {
+                // periodically check for timed-out clients
+                $now = time();
+                foreach ($this->clientActivity as $sockId => $ts) {
+                        if ($now - $ts > $this->_clientTimeout) {
+                                $client = $this->clients[(int)$sockId] ?? null;
+                                if ($client) {
+                                        $this->log("[info] Client {$client->getClientIp()} timed out (>{$this->_clientTimeout}s), disconnecting");
+                                        $client->onDisconnect();
+                                        // removeClientOnClose will be called by close()/onDisconnect(), but ensure cleanup
+                                        $this->_removeSocket($client->getClientSocket());
                                 }
-                                else
-                                {                                       
-                                        $client = $this->clients[(int)$socket];
-                                        if(!is_object($client))
-                                        {
-                                                unset($this->clients[(int)$socket]);
-                                                continue;
-                                        }
-                                        $data = $this->readBuffer($socket);                                     
-                                        $bytes = strlen($data);
-                                        
-                                        if($bytes === 0)
-                                        {
-                                                $client->onDisconnect();                                                
-                                                continue;
-                                        }
-                                        elseif($data === false)
-                                        {
-                                                $this->removeClientOnError($client);
-                                                continue;
-                                        }
-                                        elseif($client->waitingForData === false && $this->_checkRequestLimit($client->getClientId()) === false)
-                                        {
-                                                $client->onDisconnect();
-                                        }
-                                        else
-                                        {                                               
-                                                $client->onData($data);
-                                        }
-                                }
+                                unset($this->clientActivity[$sockId]);
                         }
                 }
-        }       
+
+        $changed_sockets = $this->allsockets;
+        $write = null;
+        $except = null;
+
+        $num_changed = @stream_select($changed_sockets, $write, $except, 0, 5000);
+        if ($num_changed === false) {
+            $this->log('[warn] stream_select() failed, cleaning sockets');
+            foreach ($this->clients as $key => $client) {
+                if (!isset($this->allsockets[$key]) || !is_resource($key)) {
+                    $this->removeClientOnError($client);
+                    unset($this->clients[$key]);
+                }
+            }
+            $this->allsockets = array_values(array_filter($this->allsockets, 'is_resource'));
+            continue;
+        }
+
+        foreach ($changed_sockets as $socket) {
+            if ($socket === $this->master) {
+                $ressource = @stream_socket_accept($this->master, 0);
+                if ($ressource === false) {
+                    $this->log('[warn] Socket accept failed');
+                    continue;
+                }
+                $client = $this->createConnection($ressource);
+                $this->clients[(int)$ressource] = $client;
+                $this->allsockets[] = $ressource;
+                // set initial activity timestamp
+                $this->clientActivity[(int)$ressource] = time();
+
+                if (count($this->clients) > $this->_maxClients) {
+                    $this->log('Attention: Client Limit Reached!');
+                    $client->onDisconnect();
+                    if ($this->getApplication('status') !== false) {
+                        $this->getApplication('status')->statusMsg('Attention: Client Limit Reached!', 'warning');
+                    }
+                    continue;
+                }
+
+                $this->_addIpToStorage($client->getClientIp());
+                if ($this->_checkMaxConnectionsPerIp($client->getClientIp()) === false) {
+                    $this->log('Connection/Ip limit for ip ' . $client->getClientIp() . ' was reached!');
+                    $client->onDisconnect();
+                    if ($this->getApplication('status') !== false) {
+                        $this->getApplication('status')->statusMsg('Connection/Ip limit for ip ' . $client->getClientIp() . ' was reached!', 'warning');
+                    }
+                    continue;
+                }
+            } else {
+                                $client = $this->clients[(int)$socket] ?? null;
+                if (!$client) {
+                    unset($this->clients[(int)$socket]);
+                    $this->allsockets = array_filter($this->allsockets, fn($s) => $s !== $socket);
+                    continue;
+                }
+
+                $data = $this->readBuffer($socket);
+                if ($data === false) {
+                    $this->removeClientOnError($client);
+                                        $this->_removeSocket($socket);
+                    continue;
+                }
+
+                                // update last activity timestamp on any successful read
+                                $this->clientActivity[(int)$socket] = time();
+
+                                $bytes = strlen($data);
+                if ($bytes === 0) {
+                    $client->onDisconnect();
+                                        $this->_removeSocket($socket);
+                    continue;
+                }
+
+                if ($client->waitingForData === false && $this->_checkRequestLimit($client->getClientId()) === false) {
+                    $client->onDisconnect();
+                    unset($this->clients[(int)$socket]);
+                    $this->allsockets = array_filter($this->allsockets, fn($s) => $s !== $socket);
+                    continue;
+                }
+
+                                // preserve legacy debug behavior but improve binary preview
+                                if (defined('DEBUG_WEBSOCKETS') && constant('DEBUG_WEBSOCKETS') === "1") {
+                                        if ($this->isJson($data)) {
+                                                $preview = substr($data, 0, 100);
+                                                $this->log("[debug] Received JSON from {$client->getClientIp()}: $preview (len: $bytes)");
+                                        } else {
+                                                $preview = $this->binaryPreview($data, 32);
+                                                $this->log("[debug] Received binary/non-JSON from {$client->getClientIp()}, length: $bytes, preview: $preview (Mem: " . round(memory_get_usage(true)/1024/1024, 2) . " mb)");
+                                        }
+                                } else {
+                                        // normal info logging for binary payloads
+                                        if (!$this->isJson($data)) {
+                                                $preview = $this->binaryPreview($data, 24);
+                                                $this->log("[info] Received binary/non-JSON from {$client->getClientIp()}, length: $bytes, preview: $preview (Mem: " . round(memory_get_usage(true)/1024/1024, 2) . " mb)");
+                                        }
+                                }
+
+                $client->onData($data);
+            }
+        }
+    }
+}
+
+protected function isJson($string) {
+    if (!is_string($string)) return false;
+    json_decode($string);
+    return (json_last_error() === JSON_ERROR_NONE);
+}
+
+
+
+
+protected function _removeSocket($socket)
+{
+        $key = array_search($socket, $this->allsockets, true);
+        if ($key !== false) {
+                unset($this->allsockets[$key]);
+                // reindex to keep numeric array consistent
+                $this->allsockets = array_values($this->allsockets);
+        }
+        $clientId = (int)$socket;
+        if (isset($this->clients[$clientId])) unset($this->clients[$clientId]);
+        if (isset($this->clientActivity[$clientId])) unset($this->clientActivity[$clientId]);
+        if (is_resource($socket)) {
+                @fclose($socket);
+        }
+}
+
 
         /**
          * Returns a server application.
@@ -171,7 +256,8 @@ class Server extends Socket
          */
     public function log($message, $type = 'info')
     {
-        echo date('Y-m-d H:i:s') . ' [' . ($type ? $type : 'error') . '] ' . $message . ' (Mem: '.return_memory_usage().')'. PHP_EOL;
+                $mem = round(memory_get_usage(true)/1024/1024, 2) . ' mb';
+                echo date('Y-m-d H:i:s') . ' [' . ($type ? $type : 'error') . '] ' . $message . ' (Mem: '. $mem .')'. PHP_EOL;
     }
         
         /**
@@ -191,9 +277,11 @@ class Server extends Socket
                 {
                         unset($this->_requestStorage[$clientId]);
                 }
+                // central removal helper will close socket and unset arrays
+                $this->_removeSocket($resource);
                 unset($this->clients[(int)$resource]);
-                $index = array_search($resource, $this->allsockets);
-                unset($this->allsockets[$index], $client);              
+                if (isset($this->clientActivity[(int)$resource])) unset($this->clientActivity[(int)$resource]);
+                unset($client);
                 
                 // trigger status application:
                 if($this->getApplication('status') !== false)
@@ -223,9 +311,11 @@ class Server extends Socket
                 {
                         unset($this->_requestStorage[$clientId]);
                 }
+                // central removal helper will close socket and unset arrays
+                $this->_removeSocket($resource);
                 unset($this->clients[(int)$resource]);
-                $index = array_search($resource, $this->allsockets);
-                unset($this->allsockets[$index], $client);              
+                if (isset($this->clientActivity[(int)$resource])) unset($this->clientActivity[(int)$resource]);
+                unset($client);
                 
                 // trigger status application:
                 if($this->getApplication('status') !== false)
@@ -376,7 +466,7 @@ class Server extends Socket
         /**
          * Adds a domain to the allowed origin storage.
          * 
-         * @param sting $domain A domain name from which connections to server are allowed.
+         * @param string $domain A domain name from which connections to server are allowed.
          * @return bool True if domain was added to storage.
          */
         public function setAllowedOrigin($domain)
@@ -390,6 +480,22 @@ class Server extends Socket
                 }
                 $this->_allowedOrigins[$domain] = true;         
                 return true;
+        }
+
+        /**
+         * Create a short preview for binary data: hex snippet + printable ascii
+         * @param string $data
+         * @param int $maxBytes how many bytes to preview
+         * @return string
+         */
+        protected function binaryPreview($data, $maxBytes = 32)
+        {
+                if (!is_string($data) || $data === '') return '';
+                $slice = substr($data, 0, $maxBytes);
+                $hex = bin2hex($slice);
+                // printable ascii fallback
+                $ascii = preg_replace('/[^\x20-\x7E]/', '.', $slice);
+                return sprintf('hex=%s ascii=%s%s', substr($hex, 0, 64), $ascii, (strlen($data) > $maxBytes ? '...' : ''));
         }
         
         /**
